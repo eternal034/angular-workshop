2a46e880cd1378fbdd9a3c88af3d96441057469c
Georgi Ivanov Georgiev <georgiev7900@gmail.com>
2022-03-22 13:47:51 +0200

    ~

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,43 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { ThemeComponent } from './theme/theme.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { AsideComponent } from './aside/aside.component';
+import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
+import { UserService } from './user.service';
+import { storageServiceProvider } from './storage.service';
+import { ThemeService } from './theme.service';
+import { PostService } from './post.service';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThemeComponent
+    HeaderComponent,
+    FooterComponent,
+    ThemeListComponent,
+    AsideComponent,
+    ThemeListItemComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    CoreModule
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    UserService,
+    ThemeService,
+    storageServiceProvider,
+    PostService
+  ],
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class AppModule { }
